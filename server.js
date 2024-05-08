a02c0b50a0ff51055b14b0e55aec58bab4f1e601
Ryan Pan <panryan@Ryans-MacBook-Pro.local>
2024-05-08 22:18:51 +0700

    updated code, added functionality

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const menuQuestion = [
         type: 'list',
         name: 'menuChoice',
         message: 'What would you like to do?',
-        choices: ['View All Departments', 'Add Departmenmt', 'Quit']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Quit']
     }
 ]
 
@@ -22,13 +22,63 @@ const listOptions = (response)=>{
         case 'View All Departments':
             viewDepartment();
             break;
-        case 'Add Department':
-            addDepartment();
+        case 'View All Roles':
+            viewRoles();
+            break;
+        case 'View All Employees':
+            viewEmployees();
             break;
         case 'Quit':
             mysql.end();
             break;
     }
+}
+
+const viewDepartment = () => {
+
+    mysql.query('SELECT * FROM department', function (err, results) {
+        if (err) {
+            console.error('Error fetching departments:', err);
+            return;
+        }
+
+        console.table(results);
+
+        showList();
+
+    });
+}
+
+const viewRoles = () => {
+
+    mysql.query('SELECT * FROM role', function (err, results) {
+        if (err) {
+            console.error('Error fetching roles:', err);
+            return;
+        }
+
+        console.table(results);
+
+        showList();
+
+    });
+
+}
+
+const viewEmployees = () => {
+
+    mysql.query('SELECT * FROM employee', function (err, results) {
+        if (err) {
+            console.error('Error fetching employees:', err);
+            return;
+        }
+
+        console.table(results);
+
+        showList();
+
+    });
+
 }
 
 const showList = () => {
